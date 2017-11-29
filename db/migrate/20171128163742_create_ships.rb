f8871fe5ac1a7533b2f03fa20c66e92d41213ef9
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-11-29 11:50:18 -0500

    seed file first pass

--- db/migrate/20171128163742_create_ships.rb
+++ db/migrate/20171128163742_create_ships.rb
@@ -6,6 +6,7 @@ class CreateShips < ActiveRecord::Migration[5.1]
       t.string :location
       t.references :job, foreign_key: true
       t.references :ship, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
