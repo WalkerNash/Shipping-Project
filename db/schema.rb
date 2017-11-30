7306c4b2f2cd3ccf3f878f2549cb52789889feac
PatrickColleton <patrickcolleton@gmail.com>
2017-11-30 16:28:15 -0500

    add to profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130162801) do
+ActiveRecord::Schema.define(version: 20171128191553) do
 
   create_table "jobs", force: :cascade do |t|
+    t.string "title"
     t.string "description"
     t.string "origin"
     t.string "destination"
@@ -28,10 +29,12 @@ ActiveRecord::Schema.define(version: 20171130162801) do
   create_table "ship_jobs", force: :cascade do |t|
     t.integer "job_id"
     t.integer "ship_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_ship_jobs_on_job_id"
     t.index ["ship_id"], name: "index_ship_jobs_on_ship_id"
+    t.index ["user_id"], name: "index_ship_jobs_on_user_id"
   end
 
   create_table "ships", force: :cascade do |t|
@@ -43,10 +46,6 @@ ActiveRecord::Schema.define(version: 20171130162801) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "shippic_file_name"
-    t.string "shippic_content_type"
-    t.integer "shippic_file_size"
-    t.datetime "shippic_updated_at"
     t.index ["job_id"], name: "index_ships_on_job_id"
     t.index ["ship_id"], name: "index_ships_on_ship_id"
     t.index ["user_id"], name: "index_ships_on_user_id"
