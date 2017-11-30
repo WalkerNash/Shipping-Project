ba274e5d0c691e1fbfbf5ad61bf48e696a7521a4
Raven <ravenshooper@gmail.com>
2017-11-30 11:40:48 -0500

    little changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
-  has_and_belongs_to_many :ships
+  has_and_belongs_to_many :ships, through: :user   
   # validates :name, uniqueness: true
   # validates_numericality_of :cost, greater_than: 1000
   # validates :description, length: { in: 20..1000}
