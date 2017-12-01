fda6450961d0b1f7b841a7e5f72b6dc2cff1193a
PatrickColleton <patrickcolleton@gmail.com>
2017-12-01 17:35:36 -0500

    add page functionality

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171130211137) do
 
-  create_table "feature_toggles", force: :cascade do |t|
-    t.string "name"
-    t.boolean "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "job_ships", force: :cascade do |t|
     t.integer "job_id"
     t.integer "ship_id"
@@ -44,17 +37,6 @@ ActiveRecord::Schema.define(version: 20171130211137) do
     t.index ["ship_id"], name: "index_jobs_on_ship_id"
   end
 
-  create_table "ship_jobs", force: :cascade do |t|
-    t.integer "job_id"
-    t.integer "ship_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["job_id"], name: "index_ship_jobs_on_job_id"
-    t.index ["ship_id"], name: "index_ship_jobs_on_ship_id"
-    t.index ["user_id"], name: "index_ship_jobs_on_user_id"
-  end
-
   create_table "ships", force: :cascade do |t|
     t.string "name"
     t.integer "containers"
@@ -64,10 +46,6 @@ ActiveRecord::Schema.define(version: 20171130211137) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "shippic_file_name"
-    t.string "shippic_content_type"
-    t.integer "shippic_file_size"
-    t.datetime "shippic_updated_at"
     t.index ["job_id"], name: "index_ships_on_job_id"
     t.index ["ship_id"], name: "index_ships_on_ship_id"
     t.index ["user_id"], name: "index_ships_on_user_id"
