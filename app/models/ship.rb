152886aa2e83d3cf0ed8a806f59b55377a5b9f5a
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-12-12 11:13:57 -0500

    model adjustment

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,7 +1,9 @@
 class Ship < ApplicationRecord
-  has_many :jobShips
+  has_many :job_ships
 	has_many :jobs, through: :job_ships
-  belongs_to :user
+  belongs_to :user, optional: true
+
+
   # validates :name, uniqueness: true
   # validates :location
   has_attached_file :shippic, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
