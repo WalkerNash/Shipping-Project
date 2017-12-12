cbfae408abab50f9d0a1f67c1c943f255b656bc3
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-12-12 11:13:39 -0500

    model adjustment

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
-  has_many :jobShips
-	has_many :ships, through: :user
+  has_many :job_ships
+	has_many :ships, through: :job_ships
   # validates :name, uniqueness: true
   # validates_numericality_of :cost, greater_than: 999.99
   # validates :description, length: { in: 20..1000}
