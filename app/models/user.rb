1bb1967ff46c9b8fb794eb15bbd8ae239a3325f8
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-12-12 11:14:10 -0500

    removing unnecessary model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :ships
 	has_many :jobs
-  has_many :jobShips
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
